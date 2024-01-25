3a073d4595813c0a4501bf42ca47eb1800478ce5
memikeymikey <micah_marie_cabatingan@dlsu.edu.ph>
2024-01-25 11:59:10 +0800

    first commit

---
+++ new.c
@@ -0,0 +1,23 @@
+/** FOR STUDENTS : PLEASE READ
+ * 
+ * This file is where you intend to write your code.
+ * 
+ * For this sample exercise, you need to remove the comment symbol (i.e. // ) below.
+ * Concretely, on line 14,16,17, and 19, you need to remove the comment symbol ( // )
+ * Please remove the comment symbols alone. Do not change other contents within each line.
+ * 
+*/
+#include <stdio.h>
+
+int main(void) {
+
+    int numbers;
+
+    printf("Enter number: ");
+    scanf("%d",&numbers);
+
+    printf("Hello World %d!\n",numbers);
+
+
+    return 0;
+}
